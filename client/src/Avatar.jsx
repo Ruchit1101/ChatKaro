b3de8109ce77916dd64601847de40dfe9862a01a
Ruchit Raushan <ruchitraushan@gmail.com>
2023-07-08 10:01:48 +0530

    first commit

---
+++ client/src/Avatar.jsx
@@ -0,0 +1,19 @@
+export default function Avatar({userId, username, online}){
+    const colors= ['bg-red-200', 'bg-green-200', 'bg-purple-200', 'bg-green-200', 'bg-black-200', 'bg-blue-200', 'bg-gray-200'];
+    const userIdBase10 = parseInt(userId, 16);
+    const colorIndex = userIdBase10 % colors.length;
+    const color = colors[colorIndex];
+    return(
+        <div className={"w-8 h-8 rounded-full flex items-center " + color}>
+      <div className="text-center w-full opacity-70">{username[0]}</div>
+       { online && (
+           <div className="absolute w-3 h-3  bg-green-500   rounded-full border border-white">
+           </div>
+       )}
+      {!online && (
+         <div className="absolute w-3 h-3 bg-gray-500   rounded-full border border-white">
+         </div>
+      )}
+    </div>
+    );
+}
